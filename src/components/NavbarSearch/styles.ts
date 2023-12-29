f698a74e0a7678d9791208172daefe6e361aa13f
Eric Andrade <e.andrade5f@gmail.com>
2023-12-29 14:52:39 -0300

    chore: installs font awesome

---
+++ src/components/NavbarSearch/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 5px;
+`;
+
+export const SearchInput = styled.input`
+  border: none;
+  outline: none;
+  width: 100%;
+  padding: 5px;
+`;
