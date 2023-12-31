b1c2963f82f78f40d369bc3644967d1a762f1bed
Eric Andrade <e.andrade5f@gmail.com>
2023-12-31 16:19:55 -0300

    chore: adds colors from mockup to theme file

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,21 @@
 export const theme = {
-    COLORS:{  
+    COLORS:{
+      GRAY_CARD: "#F5F5F7",
+
+      GRAY_100: "#CCC",
+      GRAY_200: "#434343",
       GRAY_300: "#707070",
+
+      //Footer only
+      GRAY_500: "#424146",
+      GRAY_600: "#323237",
+
       GRAY_800: "#2F2E41",
 
+      RED_300: "#F6303F",
       RED_500: "#D2202C",
 
+      BLACK: "#000",
       WHITE: "#FFF"
     },
     FONT_WEIGHT:{
