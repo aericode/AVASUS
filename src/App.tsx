095200721d7ddcc2131a4688db252c9661f1bce5
Eric Andrade <e.andrade5f@gmail.com>
2024-01-09 13:18:12 -0300

    chore: main branch merging

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       <>
         <ImageCarousel />
         <MainMenuEducationalModules/>
-        <MainMenuPartnersDisplay/>
+        {/* <MainMenuPartnersDisplay/> */}
 
       </>
       <Footer />
