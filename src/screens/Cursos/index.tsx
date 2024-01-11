496d4db91204ce31441596e0c99b2b78f83ec5ae
Eric Andrade <e.andrade5f@gmail.com>
2024-01-11 19:38:18 -0300

    chore: creates base for course page

--- src/screens/Cursos/index.tsx
+++ src/screens/Cursos/index.tsx
@@ -1,10 +1,23 @@
-import { Container } from './styles';
+import { Breadcrumbs } from '../../components/Breadcrumbs';
+import { Container, PaginationWrapper, StyledHeader, Wrapper } from './styles';
+import { useContext } from 'react';
+import { DataContext } from '../../contexts/DataContext';
+import { PaginatedContent } from '../../components/PaginatedContent';
 
 
 export function Cursos() {
+  const { parceirosData } = useContext(DataContext)!;
+
+
   return (
     <Container>
-      <p>cursos</p>
+      <Wrapper>
+        <Breadcrumbs rootText={'Início / Cursos /'} currentPageText={' Módulos'} />
+        <StyledHeader>Nossos parceiros</StyledHeader>
+        <PaginationWrapper>
+          <PaginatedContent paginationObjectType={'parceiro'} contentArray={parceirosData} />
+        </PaginationWrapper>
+      </Wrapper>
     </Container>
   );
 }
--- src/screens/Cursos/styles.ts
+++ src/screens/Cursos/styles.ts
@@ -2,5 +2,26 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
+  margin-top: 30px;
+  margin-bottom: 190px;
 `;
+
+export const Wrapper = styled.div`
+  width: 80vw;
+  max-width: 1200px;
+`;
+
+export const PaginationWrapper = styled.div`
+  margin: 30px 0;
+`
+
+export const StyledHeader = styled.h2`
+  color: ${({ theme }) => theme.COLORS.RED_500};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL3};
+  margin: 30px 0;
+  text-align: center;
+`
