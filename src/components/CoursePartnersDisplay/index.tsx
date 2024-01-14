384604a0a6d32b611591523ad638a88b193f1307
Eric Andrade <e.andrade5f@gmail.com>
2024-01-14 17:23:16 -0300

    feat: updetes partner display grid

--- src/components/CoursePartnersDisplay/index.tsx
+++ src/components/CoursePartnersDisplay/index.tsx
@@ -1,18 +1,26 @@
+import { useScreen } from "../../contexts/ScreenSizeContext";
 import { Curso } from "../../types/curso"
-import { Container, PartnerList, PartnerListImage } from "./styles";
+import { ImageWithSpinner } from "../ImageWithSpinner";
+import { Container, PartnerGrid, PartnerGridImage, PartnerList, PartnerListImage } from "./styles";
 
 interface CoursePartnersDisplayProps{
   curso:Curso
 }
 
 export function CoursePartnersDisplay({curso}:CoursePartnersDisplayProps){
+  const { isSmallScreen } = useScreen();
 
   return(
     <Container>
+      {isSmallScreen ?
       <PartnerList>
-        {curso.creditos.map(partner=> <PartnerListImage width={"200px"}  height={"auto"} src={partner.capa} title={partner.titulo} />)}
+        {curso.creditos.map(partner=> <ImageWithSpinner width={"240px"}  height={"auto"} src={partner.capa} title={partner.titulo} isImageContain={true}/>)}
       </PartnerList>
-      
+      :
+      <PartnerGrid>
+        {curso.creditos.map(partner=> <ImageWithSpinner width={"200px"}  height={"auto"} src={partner.capa} title={partner.titulo} isImageContain={true}/>)}
+      </PartnerGrid>
+      }
     </Container>
     
   )
